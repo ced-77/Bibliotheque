243c4323b31864a10eebc597e94b4b2a9ef3400e
Ced-77 <cedric.ray@hotmail.com>
2019-04-19 02:26:02 +0200

    creation de la vue de l'accueil qui détermine les vues générales du sites

---
+++ src/Vue/FrameAccueil.java
@@ -0,0 +1,29 @@
+package Vue;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class FrameAccueil extends JFrame {
+
+	private JPanel contentPane;
+
+	
+
+	/**
+	 * Creation de la Frame d'accueil qui sert à determiner l'emplacement
+	 * de la fenêtre du site.
+	 */
+	public FrameAccueil() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
